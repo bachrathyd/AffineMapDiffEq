b49eb72a8b7096fd32c4e8a7e8cfd13042ceb195
BachrathyD <bachrathy@mm.bme.hu>
2024-04-04 19:02:44 +0200

    3D chart - LogDek
    Working - but not precise

--- KF_valve_time_sim_explicit.jl
+++ KF_valve_time_sim_explicit.jl
@@ -45,7 +45,7 @@ function valve_KF_DADE_exp_FIXPOINT(y, p)
 end
 
 
-function y4_fun(sol)
+function y4_fun(sol, p)
     ζ, δ, τ, β, Φ, q, _ = deepcopy(sol.prob.p)
     y4series = [
         -sol.prob.p[end](p, t - τ) - (Φ^2.0 * sol[1, it]^2.0) / 2.0 +
@@ -60,135 +60,157 @@ function y4_fun(sol)
 
     return h4_interp #, y4series
 end
-
-
 ## - futtatás 
+
+x = []
+y = []
+z = []
+
+scatter([], [], [])
 ζ = 0.39
 δ = 3.0
 τ = 0.25
-β = 7.78
-Φ = 48.2
-#q = 3.0;
-q = 10.0
+#for β = LinRange(7.6,7.9,10)#7.82
+for β = LinRange(7.79, 7.815, 50)#7.82
+    println("---------------")#7.82
+    println(β)
+    Φ = 48.2
+    #q = 3.0;
+    q = 10.0
 
-p_FIX = (ζ, δ, τ, β, Φ, q, "semmise")
-T = τ
+    p_FIX = (ζ, δ, τ, β, Φ, q, "semmise")
+    T = τ
 
-using NonlinearSolve
+    using NonlinearSolve
 
-u0_FIX = SA[2.0, 0.0, 2.5, q*Φ/2]
-valve_KF_DADE_exp_FIXPOINT(u0_FIX, p_FIX)
-probFIX = NonlinearSolve.NonlinearProblem(valve_KF_DADE_exp_FIXPOINT, u0_FIX, p_FIX)
-@show uFIX = NonlinearSolve.solve(probFIX, NewtonRaphson())
-
-
-scatter()
-for Nmutip in 2.3:-0.05:0.01#0.1:0.1:2
-    println("Nmutip: $Nmutip")
-    Base.:+(a::SVector, b::Bool) = a .+ b
-
-    #h123(p, t) = SA[2.0, 0.0, 2.5]
-    u0start=[4.42*Nmutip, 0.0, 2.5*Nmutip] .* 0.0 .+ uFIX[1:3] .*Nmutip#+rand(3).*Nmutip
-    h123(p, t) = SA[u0start...]
-    h4(p, t) = q * Φ / 2 * Nmutip* 0.0  + uFIX[4]*Nmutip
+    u0_FIX = SA[2.0, 0.0, 2.5, q*Φ/2]
+    valve_KF_DADE_exp_FIXPOINT(u0_FIX, p_FIX)
+    probFIX = NonlinearSolve.NonlinearProblem(valve_KF_DADE_exp_FIXPOINT, u0_FIX, p_FIX)
+    @show uFIX = NonlinearSolve.solve(probFIX, NewtonRaphson())
 
 
-    p = (ζ, δ, τ, β, Φ, q, h4)
-    u0 = h123(p, 0.0)
-    valve_KF_DADE_exp_fun = DDEFunction(valve_KF_DADE_exp)
 
-    #kt=0
-    #prob_valve_exp = DDEProblem(valve_KF_DADE_exp_fun, u0, h123, [0.0, T] .+ kt * τ, p, constant_lags=[τ])
-    #integrator = init(prob_valve_exp, MethodOfSteps(Rodas5()),reltol=1e-8, abstol=1e-8)
-    #
-    #step!(integrator, τ) 
-    #plot(integrator.sol)
-    #
-    #h44 = y4_fun(integrator.sol)
-    #integrator.p = (ζ, δ, τ, β, Φ, q, h44)
-    #check_error(integrator)
+    for Nmutip in 1.55:-0.05:1.05#0.1:0.1:2
+        println("Nmutip: $Nmutip")
+        Base.:+(a::SVector, b::Bool) = a .+ b
 
-    t_final = [0.0]
-    u_final = [u0]
-    f_final = [h4(p, 0.0)]
+        #h123(p, t) = SA[2.0, 0.0, 2.5]
+        u0start = [4.42 * Nmutip, 0.0, 2.5 * Nmutip] .* 0.0 .+ uFIX[1:3] .* Nmutip#+rand(3).*Nmutip
+        h123(p, t) = SA[u0start...]
+        h4(p, t) = q * Φ / 2 * Nmutip * 0.0 + uFIX[4] * Nmutip
 
-    sizehint!(t_final, 1_000_000)
-    sizehint!(u_final, 1_000_000)
-    sizehint!(f_final, 1_000_000)
-    kt = 0
-    #kt=10000
-    #for kt in 0:10000
-    for _ in 0:1000
-        kt += 1
-        if mod(kt, 50) == 0
-            println(kt)
+
+        p = (ζ, δ, τ, β, Φ, q, h4)
+        u0 = h123(p, 0.0)
+        valve_KF_DADE_exp_fun = DDEFunction(valve_KF_DADE_exp)
+
+        #kt=0
+        #prob_valve_exp = DDEProblem(valve_KF_DADE_exp_fun, u0, h123, [0.0, T] .+ kt * τ, p, constant_lags=[τ])
+        #integrator = init(prob_valve_exp, MethodOfSteps(Rodas5()),reltol=1e-8, abstol=1e-8)
+        #
+        #step!(integrator, τ) 
+        #plot(integrator.sol)
+        #
+        #h44 = y4_fun(integrator.sol,p)
+        #integrator.p = (ζ, δ, τ, β, Φ, q, h44)
+        #check_error(integrator)
+
+        t_final = [0.0]
+        u_final = [u0]
+        f_final = [h4(p, 0.0)]
+
+        sizehint!(t_final, 1_000_000)
+        sizehint!(u_final, 1_000_000)
+        sizehint!(f_final, 1_000_000)
+        kt = 0
+        #kt=10000
+        #for kt in 0:10000
+        for _ in 0:5000
+            kt += 1
+            #if mod(kt, 50) == 0
+            #    println(kt)
+            #end
+            prob_valve_exp = DDEProblem(valve_KF_DADE_exp_fun, u0, h123, [0.0, T] .+ kt * τ, p, constant_lags=[τ])
+
+            #@time 
+            sol_exp = solve(prob_valve_exp, MethodOfSteps(Rodas5()), reltol=1e-10, abstol=1e-10)
+            #sol_exp = solve(prob_valve_exp, MethodOfSteps(BS3()),        reltol=1e-5, abstol=1e-5,dt=0.5)
+
+            #plot!(sol_exp, linestyle=:dash)
+
+            h44 = y4_fun(sol_exp, p)
+            fser = [h44(p, t) for t in sol_exp.t]
+            #PLOTFIG=plot!(sol_exp.t, fser, linestyle=:dash,xlim=(0,sol_exp.t[end]),linewidth = 2)
+
+            p = (ζ, δ, τ, β, Φ, q, h44)
+            u0 = sol_exp[end]
+
+            append!(t_final, sol_exp.t)
+            append!(u_final, sol_exp.u)
+            append!(f_final, fser)
+
+            if false#mod(kt, 100) == 0
+                println(kt)
+                y1 = getindex.(u_final, 1)
+                #plot(t_final,y1,linewidth = 2)
+                MaxInd = argmaxima(y1)
+                y1locmax = y1[MaxInd]
+                pp = scatter(t_final[MaxInd], y1[MaxInd], color=:red)
+                display(pp)
+            end
         end
-        prob_valve_exp = DDEProblem(valve_KF_DADE_exp_fun, u0, h123, [0.0, T] .+ kt * τ, p, constant_lags=[τ])
 
-        #@time 
-        sol_exp = solve(prob_valve_exp, MethodOfSteps(Rodas5()), reltol=1e-10, abstol=1e-10)
-        #sol_exp = solve(prob_valve_exp, MethodOfSteps(BS3()),        reltol=1e-5, abstol=1e-5,dt=0.5)
-
-        #plot!(sol_exp, linestyle=:dash)
-
-        h44 = y4_fun(sol_exp)
-        fser = [h44(p, t) for t in sol_exp.t]
-        #PLOTFIG=plot!(sol_exp.t, fser, linestyle=:dash,xlim=(0,sol_exp.t[end]),linewidth = 2)
-
-        p = (ζ, δ, τ, β, Φ, q, h44)
-        u0 = sol_exp[end]
-
-        append!(t_final, sol_exp.t)
-        append!(u_final, sol_exp.u)
-        append!(f_final, fser)
-
-        if false#mod(kt, 100) == 0
-            println(kt)
-            y1 = getindex.(u_final, 1)
-            #plot(t_final,y1,linewidth = 2)
-            MaxInd = argmaxima(y1)
-            y1locmax = y1[MaxInd]
-            pp = scatter(t_final[MaxInd], y1[MaxInd], color=:red)
-            display(pp)
-        end
-    end
-
-    #file = matopen("matfile.mat", "w")
-    #write(file, "u_final", u_final)
-    #write(file, "f_final", f_final)
-    #close(file)
+        #file = matopen("matfile.mat", "w")
+        #write(file, "u_final", u_final)
+        #write(file, "f_final", f_final)
+        #close(file)
 
 
-    #plot(t_final,getindex.(u_final,1), linestyle=:dash,linewidth = 3)
-    #plot!(t_final,getindex.(u_final,2), linestyle=:dash,linewidth = 3)
-    #plot!(t_final,getindex.(u_final,3), linestyle=:dash,linewidth = 3)
-    #plot!(t_final,f_final, linestyle=:dash,linewidth = 3)
+        #plot(t_final,getindex.(u_final,1), linestyle=:dash,linewidth = 3)
+        #plot!(t_final,getindex.(u_final,2), linestyle=:dash,linewidth = 3)
+        #plot!(t_final,getindex.(u_final,3), linestyle=:dash,linewidth = 3)
+        #plot!(t_final,f_final, linestyle=:dash,linewidth = 3)
 
-    Npoints = size(t_final, 1)
-    t_final = t_final[floor(Int, Npoints / 2):end]
-    u_final = u_final[floor(Int, Npoints / 2):end]
-    f_final = f_final[floor(Int, Npoints / 2):end]
+        Npoints = size(t_final, 1)
+        t_final = t_final[floor(Int, Npoints / 10):end]
+        u_final = u_final[floor(Int, Npoints / 10):end]
+        f_final = f_final[floor(Int, Npoints / 10):end]
 
-    y1 = getindex.(u_final, 1)
-    y1_pert = y1 .- uFIX[1]
+        y1 = getindex.(u_final, 1)
+        y1_pert = y1 .- uFIX[1]
 
-    #plot(t_final, y1_pert, linewidth=2)
+        #plot(t_final, y1_pert, linewidth=2)
 
-    MaxInd = argmaxima(y1)
-    y1locmax = y1_pert[MaxInd]
+        MaxInd = argmaxima(y1)
+        y1locmax = y1_pert[MaxInd]
 
-    #scatter!(t_final[MaxInd], y1[MaxInd])
+        #scatter!(t_final[MaxInd], y1[MaxInd])
 
-    #scatter(t_final[MaxInd][2:end],log.(y1locmax[1:end-1]./y1locmax[2:end]),yticks=LinRange(0,1e-2,10))
+        #scatter(t_final[MaxInd][2:end],log.(y1locmax[1:end-1]./y1locmax[2:end]),yticks=LinRange(0,1e-2,10))
 
+        #2D
+        #  PLOTaaa = scatter!(log.(y1locmax[1:end-1] ./ y1locmax[2:end]),        y1locmax[1:end-1] .+ uFIX[1], ylabel="y1", xlabel="LogDek")
+        #3D 
+        #  PLOTaaa = scatter!(y1locmax[1:end-1]*0.0 .+ β, y1locmax[1:end-1] .+ uFIX[1],log.(y1locmax[1:end-1] ./ y1locmax[2:end]),
+        #      xlabel="β", ylabel="y1",zlabel="LogDek")
 
-    PLOTaaa = scatter!(log.(y1locmax[1:end-1] ./ y1locmax[2:end]),
-        y1locmax[1:end-1] .+ uFIX[1]*0.0, ylabel="y1", xlabel="LogDek")
-        #xlim=(0, 1e-2),
+        append!(x, y1locmax[1:end-1] * 0.0 .+ β)
+        append!(y, y1locmax[1:end-1] .+ uFIX[1])
+        append!(z, log.(y1locmax[1:end-1] ./ y1locmax[2:end]))
+        #xlim=(0, 1e-2))
         #ylim=(0, 7))
 
-    #xticks=LinRange(0, 1e-2, 11),
-    display(PLOTaaa)
+        #xticks=LinRange(0, 1e-2, 11),
+        # display(PLOTaaa)
+
+    end
+    #plot!([0,0.003],[1.0,1.0] .* uFIX[1], linestyle=:dash,linewidth = 3,title="β: $β",        xlim=(-0.01, 1e-2))
 
 end
-scatter!()
+gr()
+theme(:dao)
+foo(x) = (x > 0) ? (x) + 0.02 : 0
+#foo(x)= x
+scatter(x, y; zcolor=foo.(z))
+
+
