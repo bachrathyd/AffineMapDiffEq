e44b9f66714b1039ccc068908769a3d4595fab62
BachrathyDaniel <bachrathyd@gmail.com>
2024-04-10 13:56:55 +0200

    Bifurcation?plot

--- KF_valve_time_sim_explicit.jl
+++ KF_valve_time_sim_explicit.jl
@@ -2,7 +2,6 @@
 # Algeb. Delay Diff. Eq.
 5 + 5
 
-
 using Revise
 #using DDE_mapping
 
@@ -12,6 +11,7 @@ using LinearAlgebra
 using BenchmarkTools
 using Plots
 plotly()
+gr()
 using Profile
 using StaticArrays
 using DifferentialEquations
@@ -20,7 +20,7 @@ using LaTeXStrings
 # using Memoization
 
 using MDBM
-#using MAT
+using MAT
 using Peaks
 using NaNStatistics
 
@@ -70,12 +70,15 @@ z = Float64[]
 
 logdekmin = Float64[]
 logdekminY1 = Float64[]
+logdekminbeta = Float64[]
 scatter(Float64[], Float64[], Float64[])
 ζ = 0.39
 δ = 3.0
 τ = 0.25
-#for β = LinRange(7.6,7.9,10)#7.82
-for β = LinRange(7.5, 7.815, 30)#7.82
+for β = LinRange(5,9,40)#7.82
+#for β = LinRange(7.0, 7.9, 30)#7.82
+#for β = LinRange(7.72, 7.82, 30)#7.82
+#for β = LinRange(7.79, 7.803,40)#7.82
     #for β = LinRange(7.79, 7.815, 50)#7.82
 
     xx = Float64[]
@@ -99,7 +102,7 @@ for β = LinRange(7.5, 7.815, 30)#7.82
 
 
 
-    for Nmutip in 1.55:-0.025:1.05#0.1:0.1:2
+    for Nmutip in 1.95:-0.205:1.02#1.75:-0.01:1.05#0.1:0.1:2
         println("Nmutip: $Nmutip")
         Base.:+(a::SVector, b::Bool) = a .+ b
 
@@ -134,7 +137,7 @@ for β = LinRange(7.5, 7.815, 30)#7.82
         kt = 0
         #kt=10000
         #for kt in 0:10000
-        for _ in 0:1000
+        for _ in 0:1000#250#2500
             kt += 1
             #if mod(kt, 50) == 0
             #    println(kt)
@@ -142,7 +145,9 @@ for β = LinRange(7.5, 7.815, 30)#7.82
             prob_valve_exp = DDEProblem(valve_KF_DADE_exp_fun, u0, h123, [0.0, T] .+ kt * τ, p, constant_lags=[τ])
 
             #@time 
-            sol_exp = solve(prob_valve_exp, MethodOfSteps(Rodas5()), reltol=1e-12, abstol=1e-12)
+            #sol_exp = solve(prob_valve_exp, MethodOfSteps(Rodas5()), reltol=1e-14, abstol=1e-14)
+            #sol_exp = solve(prob_valve_exp, MethodOfSteps(Rodas5()), reltol=1e-11, abstol=1e-11)
+            sol_exp = solve(prob_valve_exp, MethodOfSteps(Rodas5()), reltol=1e-8, abstol=1e-8)
             #sol_exp = solve(prob_valve_exp, MethodOfSteps(BS3()),        reltol=1e-5, abstol=1e-5,dt=0.5)
 
             #plot!(sol_exp, linestyle=:dash)
@@ -175,17 +180,18 @@ for β = LinRange(7.5, 7.815, 30)#7.82
         #close(file)
 
 
+        Npoints = size(t_final, 1)
+
+        t_final = t_final[floor(Int, Npoints * 0.6):end]
+        u_final = u_final[floor(Int, Npoints * 0.6):end]
+        f_final = f_final[floor(Int, Npoints * 0.6):end]
 
         #plotly()
-        # aa=       plot(t_final,getindex.(u_final,1),linewidth = 3)#, linestyle=:dash
+        #plot(t_final, getindex.(u_final, 1), linewidth=3)#, linestyle=:dash
         #        plot!(t_final,getindex.(u_final,2),linewidth = 3)#, linestyle=:dash
         #        plot!(t_final,getindex.(u_final,3),linewidth = 3)#, linestyle=:dash
         #        aa=plot!(t_final,f_final,linewidth = 3)#, linestyle=:dash
-        #display(aa)
-        Npoints = size(t_final, 1)
-        t_final = t_final[floor(Int, Npoints / 5):end]
-        u_final = u_final[floor(Int, Npoints / 5):end]
-        f_final = f_final[floor(Int, Npoints / 5):end]
+
 
         y1 = getindex.(u_final, 1)
         y1_pert = y1 .- uFIX[1]
@@ -197,8 +203,8 @@ for β = LinRange(7.5, 7.815, 30)#7.82
 
 
         logdek = log.(y1locmax[1:end-1] ./ y1locmax[2:end])
-        #scatter!(t_final[MaxInd], y1[MaxInd])
-
+        # aa = scatter!(t_final[MaxInd], y1[MaxInd])
+        # display(aa)
         #scatter(t_final[MaxInd][2:end],log.(y1locmax[1:end-1]./y1locmax[2:end]),yticks=LinRange(0,1e-2,10))
 
         #2D
@@ -206,10 +212,16 @@ for β = LinRange(7.5, 7.815, 30)#7.82
         #3D 
         #  PLOTaaa = scatter!(y1locmax[1:end-1]*0.0 .+ β, y1locmax[1:end-1] .+ uFIX[1],logdek,
         #      xlabel="β", ylabel="y1",zlabel="LogDek")
-        append!(xx, y1locmax[1:end-1] * 0.0 .+ β)
-        append!(yy, y1locmax[1:end-1] .+ uFIX[1])
-        append!(zz, logdek)
 
+        if (true)
+            append!(xx, y1locmax[1:end-1] * 0.0 .+ β)
+            append!(yy, y1locmax[1:end-1] .+ uFIX[1])
+            append!(zz, logdek)
+        else
+            append!(xx, β)
+            append!(yy, mean(y1locmax[1:end-1]) .+ uFIX[1])
+            append!(zz, mean(logdek))
+        end
         #xlim=(0, 1e-2))
         #ylim=(0, 7))
 
@@ -219,31 +231,95 @@ for β = LinRange(7.5, 7.815, 30)#7.82
     end
 
     Imin = argmin(zz)
-    Nsmmoth=5
+    Nsmmoth = 1
     zzSmoth = movmean(zz, Nsmmoth)
-    append!(logdekmin,zzSmoth[Imin])
-    append!(logdekminY1,yy[Imin])
+    Imin = argmin(zzSmoth)
+    append!(logdekmin, zzSmoth[Imin])
+    append!(logdekminY1, yy[Imin])
+    append!(logdekminbeta, β)
+
 
     append!(x, xx)
     append!(y, yy)
     append!(z, zz)
     #plot!([0,0.003],[1.0,1.0] .* uFIX[1], linestyle=:dash,linewidth = 3,title="β: $β",        xlim=(-0.01, 1e-2))
 
+
+    plotly()
     theme(:dao)
     foo(x) = (x > 0) ? (x) + 0.02 : 0
     #foo(x)= x
     #  BBplot=scatter(x, y; zcolor=foo.(z))
 
-    BBplot = scatter(y, z; zcolor=foo.(x))
-    plot!= scatter(yy, zz)
-    plot!(logdekminY1, logdekmin, linewidth=3)
+    scatter(y, z; zcolor=foo.(x))
+    plot!(yy, zzSmoth)
 
-    display(BBplot)
+    BBplot = plot!(logdekminY1, logdekmin, linewidth=3)
+
+
+    ##Linear fit
+    beta = sum((logdekminbeta .- mean(logdekminbeta)) .* (logdekmin .- mean(logdekmin))) /
+           sum((logdekminbeta .- mean(logdekminbeta)) .* (logdekminbeta .- mean(logdekminbeta)))
+    alfa = mean(logdekmin) - beta * mean(logdekminbeta)
+    scatter(logdekminbeta, logdekmin, linewidth=3)
+    plot!([minimum(logdekminbeta), 7.9], [minimum(logdekminbeta), 7.9] * beta .+ alfa)
+    AAplot = scatter!([-alfa / beta], [0], title=-alfa / beta)
+
+    display(plot(AAplot, BBplot))
+
 end
+
+theme(:dao)
+foo(x) = (x > 0) ? (x) + 0.02 : 0
+
+foo(x) = (x > 0) ? (x) : (NaN)
+#foo(x)= x
+#  BBplot=scatter(x, y; zcolor=foo.(z))
+
+scatter(y, z; zcolor=foo.(x))
+BBplot = plot!(logdekminY1, logdekmin, linewidth=3, xlabel="Y_1", ylabel="Λ_min")
+
+##Linear fit
+beta = sum((logdekminbeta .- mean(logdekminbeta)) .* (logdekmin .- mean(logdekmin))) /
+       sum((logdekminbeta .- mean(logdekminbeta)) .* (logdekminbeta .- mean(logdekminbeta)))
+alfa = mean(logdekmin) - beta * mean(logdekminbeta)
+
+scatter(logdekminbeta, logdekmin, linewidth=3)
+plot!([minimum(logdekminbeta), 7.9], [minimum(logdekminbeta), 7.9] * beta .+ alfa)
+AAplot = scatter!([-alfa / beta], [0], title=-alfa / beta, xlabel="β", ylabel="Λ_min")
+
+display(plot(BBplot, AAplot))
+
+
+##
+file = matopen("q10_bif.mat")
+xH = read(file, "xH")
+yH = read(file, "yH")
+xFup = read(file, "xFup")
+yFup = read(file, "yFup")
+xFdown = read(file, "xFdown")
+yFdown = read(file, "yFdown")
+xFix = read(file, "xFix")
+yFix = read(file, "yFix")
+xFup2 = read(file, "xFup2")
+yFup2 = read(file, "yFup2")
+xFdown2 = read(file, "xFdown2")
+yFdown2 = read(file, "yFdown2")
+# note that this does NOT introduce a variable ``varname`` into scope
+close(file)
+
 
 
 plotly()
-BBplot = scatter(y, z; zcolor=foo.(x))
-plot!(logdekminY1, logdekmin, linewidth=3)
-
-plot()
+#gr()
+#scatter(x, y; zcolor=foo.(z))
+Ndrop = 1
+foo(x) = (x > 0) ? log(x) : log(x)
+scatter((x[z.>0.0][1:Ndrop:end]), (y[z.>0.0][1:Ndrop:end]); zcolor=foo.(z[z.>0.0][1:Ndrop:end]))
+scatter!([xH], [yH])
+plot!(xFup[:], yFup[:])
+plot!(xFdown[:], yFdown[:])
+plot!(xFix[:], yFix[:])
+plot!(xFup2[:], yFup2[:])
+plot!(xFdown2[:], yFdown2[:])
+plot!(logdekminbeta, logdekminY1)
